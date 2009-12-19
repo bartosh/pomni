066edf597334e0975a7ea22becad6a005693c38d
Ed Bartosh <bartosh@gmail.com>
2009-12-19 13:08:59 +0200

    refactored CurrentCard to use new base class

--- mnemosyne/mnemosyne/libmnemosyne/statistics_pages/current_card.py
+++ mnemosyne/mnemosyne/libmnemosyne/statistics_pages/current_card.py
@@ -9,13 +9,47 @@ from mnemosyne.libmnemosyne.statistics_page import HtmlStatisticsPage
 
 DAY = 24 * 60 * 60 # Seconds in a day.
 
+class CurrentCardStatPage(StatisticsPage):
 
-class CurrentCard(HtmlStatisticsPage):
+    """A statistics for current card
+
+    """
+
+    def __init__(self, component_manager):
+        StatisticsPage.__init__(self, component_manager)
+
+    def get_data(self):
+        """Get statistics data."""
+
+        card = self.review_controller().card
+        if not card:
+            self._data["error"] = _("No current card.")
+        elif card.grade == -1:
+            self._data["error"] = _("Unseen card, no statistics available yet.")
+        else:
+            db = self.database()
+            self._data["grade"] = card.grade 
+            self._data["easiness"] = card.easiness 
+            self._data["repetitions"] = (card.acq_reps + card.ret_reps)
+            self._data["lapses"] = card.lapses
+            self._data["interval"] = card.interval / DAY
+            self._data["last_repetition"] =  time.gmtime(card.last_rep)
+            self._data["next_repetition"] = time.gmtime(card.next_rep)
+            self._data["avg_thinking_time"] = db.average_thinking_time(card)
+            self._data["total_thinking_time"] = db.total_thinking_time(card)
+        return self._data
+
+
+class CurrentCard(StatisticsPage):
 
     name = _("Current card")
+
+    def __init__(self, component_manager):
+        StatisticsPage.__init__(self, component_manager)
+        self.html = None
         
     def prepare_statistics(self, variant):
-        card = self.review_controller().card
+        data = self.get_data()
         self.html = """<html<body>
         <style type="text/css">
         table { height: 100%;
@@ -26,24 +60,23 @@ class CurrentCard(HtmlStatisticsPage):
                 padding: 0;
                 border: thin solid #8F8F8F; }
         </style></head><table><tr><td>"""
-        if not card:
-            self.html += _("No current card.")
-        elif card.grade == -1:
-            self.html += _("Unseen card, no statistics available yet.")
+        if "error" in data:
+            self.html += data["error"]
         else:
-            self.html += _("Grade") + ": %d<br>" % card.grade
-            self.html += _("Easiness") + ": %1.2f<br>" % card.easiness
-            self.html += _("Repetitions") + ": %d<br>" \
-                % (card.acq_reps + card.ret_reps)
-            self.html += _("Lapses") + ": %d<br>" % card.lapses
-            self.html += _("Interval") + ": %d<br>" \
-                % (card.interval / DAY)
+            self.html += _("Grade") + ": %d<br>" % data["grade")]
+            self.html += _("Easiness") + ": %1.2f<br>" % data["easiness"]
+            self.html += _("Repetitions") + ": %d<br>" % data["repetitions"]
+            self.html += _("Lapses") + ": %d<br>" % data["lapses"]
+            self.html += _("Interval") + ": %d<br>" % data["interval"]
             self.html += _("Last repetition") + ": %s<br>" \
-                % time.strftime("%B %d, %Y", time.gmtime(card.last_rep))           
+                % time.strftime("%B %d, %Y", data["last_repetition"])           
             self.html += _("Next repetition") + ": %s<br>" \
-                % time.strftime("%B %d, %Y", time.gmtime(card.next_rep))
+                % time.strftime("%B %d, %Y", data["next_repetition"])
             self.html += _("Average thinking time (secs)") + ": %d<br>" \
-                % self.database().average_thinking_time(card)
+                % data["avg_thinking_time"]
             self.html += _("Total thinking time (secs)") + ": %d<br>" \
-                % self.database().total_thinking_time(card)
-        self.html += "</td></tr></table></body></html>"
+                % data["total_thinking_time"]
+            self.html += "</td></tr></table></body></html>"
+
+        return self.html
+
